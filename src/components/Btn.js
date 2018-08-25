98830df3dd79806ededa5923a39f5dd45b78cbc3
Gohar Melikyan <goohar.mlkyn@gmail.com>
2018-08-25 20:43:14 +0400

    added fontawesom, react-icons and navbar

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
+import { Button} from 'reactstrap';
 
 export default class Btn extends Component {
 
     constructor() {
         super();
     }
-<<<<<<< HEAD
-    
     render() {
         return (
-            <button type='submit' className='btn2'>{this.props.btnTxt}</button>
-=======
-
-    sub() {
-        document.getElementById('myForm').submit();
-    }
-
-    render() {
-        return (
-            <button type='button' onClick={sub} className='btn2'>{this.props.btnTxt}</button>
->>>>>>> 6793702a416cca6f8f21006d513f61f80fed8179
-        );
+            <Button type='submit' className='btn2'>{this.props.btnTxt}</Button>);
     }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 
+
 import Btn from './Btn';
+import {Form, Input, Button } from 'reactstrap';
 
-export default class Form extends Component {
-
+export default class MyForm extends Component {
     constructor() {
         super();
     }
 
     render() {
-        return (
-            <span>
-                <form id='myForm'>
+        return ( <Form id='myForm'>
                     <span className='form'>
                         <h2>delivery address</h2>
-
-                        <input pattern="[A-Za-z]" required type="text" placeholder="name" /><br /><br />
-                        <input pattern="[0-9]" required type="text" placeholder="phone" /><br /><br />
-                        <input required className="address" type="text" placeholder="address" />
-                        <button type="button" className="btn">Open Map</button>
+                        <Input pattern="[A-Za-z]" required type="text" placeholder="name" /><br /><br />
+                        <Input pattern="[0-9]" required type="text" placeholder="phone" /><br /><br />
+                        <Input required className="address" type="text" placeholder="address" />
+                        <Button type="button" className="btn">Open Map</Button>
                     </span>
                     <div className='text-center'>
                         <Btn btnTxt='Make pizza' />
                     </div>
-                </form>
-            </span>
+                </Form>
+
         );
     }
 }
